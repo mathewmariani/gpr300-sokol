655431a12010b07a5db363a1f9b6be11b1692e64
Mat Mariani <someone.mariani@gmail.com>
2024-02-21 23:23:51 -0500

    Added LICENSE to libs

--- libs/sokol/update.sh
+++ libs/sokol/update.sh
@@ -5,15 +5,16 @@
 SOKOL_PREFIX="https://raw.githubusercontent.com/floooh/sokol/master"
 
 # headers
-SOKOL_APP="$SOKOL_PREFIX/sokol_app.h"
-SOKOL_FETCH="$SOKOL_PREFIX/sokol_fetch.h"
-SOKOL_GFX="$SOKOL_PREFIX/sokol_gfx.h"
-SOKOL_GLUE="$SOKOL_PREFIX/sokol_glue.h"
-SOKOL_LOG="$SOKOL_PREFIX/sokol_log.h"
-SOKOL_TIME="$SOKOL_PREFIX/sokol_time.h"
+SOKOL_APP_H="$SOKOL_PREFIX/sokol_app.h"
+SOKOL_FETCH_H="$SOKOL_PREFIX/sokol_fetch.h"
+SOKOL_GFX_H="$SOKOL_PREFIX/sokol_gfx.h"
+SOKOL_GLUE_H="$SOKOL_PREFIX/sokol_glue.h"
+SOKOL_LOG_H="$SOKOL_PREFIX/sokol_log.h"
+SOKOL_TIME_H="$SOKOL_PREFIX/sokol_time.h"
 
 # utils
-SOKOL_IMGUI="$SOKOL_PREFIX/util/sokol_imgui.h"
+SOKOL_IMGUI_H="$SOKOL_PREFIX/util/sokol_imgui.h"
+SOKOL_SHAPE_H="$SOKOL_PREFIX/util/sokol_shape.h"
 
 # license
 SOKOL_LICENSE="$SOKOL_PREFIX/LICENSE"
@@ -22,15 +23,16 @@ SOKOL_LICENSE="$SOKOL_PREFIX/LICENSE"
 OUTPUT=./libs/sokol
 
 # curl all headers and utils
-curl $SOKOL_APP > $OUTPUT/sokol_app.h
-curl $SOKOL_FETCH > $OUTPUT/sokol_fetch.h
-curl $SOKOL_GFX > $OUTPUT/sokol_gfx.h
-curl $SOKOL_GLUE > $OUTPUT/sokol_glue.h
-curl $SOKOL_LOG > $OUTPUT/sokol_log.h
-curl $SOKOL_TIME > $OUTPUT/sokol_time.h
+curl $SOKOL_APP_H > $OUTPUT/sokol_app.h
+curl $SOKOL_FETCH_H > $OUTPUT/sokol_fetch.h
+curl $SOKOL_GFX_H > $OUTPUT/sokol_gfx.h
+curl $SOKOL_GLUE_H > $OUTPUT/sokol_glue.h
+curl $SOKOL_LOG_H > $OUTPUT/sokol_log.h
+curl $SOKOL_TIME_H > $OUTPUT/sokol_time.h
 
 # utils
-curl $SOKOL_IMGUI > $OUTPUT/sokol_imgui.h
+curl $SOKOL_IMGUI_H > $OUTPUT/sokol_imgui.h
+curl $SOKOL_SHAPE_H > $OUTPUT/sokol_shape.h
 
 # license
 curl $SOKOL_LICENSE > $OUTPUT/LICENSE
--- libs/stb/update.sh
+++ libs/stb/update.sh
@@ -2,13 +2,17 @@
 # update the required stb headers
 
 # repo url
-SOKOL_PREFIX="https://raw.githubusercontent.com/nothings/stb/master"
+STB_PREFIX="https://raw.githubusercontent.com/nothings/stb/master"
 
 # headers
-STB_IMAGE="$SOKOL_PREFIX/stb_image.h"
+STB_IMAGE_H="$STB_PREFIX/stb_image.h"
+
+# license
+STB_LICENSE="$STB_PREFIX/LICENSE"
 
 # output directory
-OUTPUT=./engine/libs/stb
+OUTPUT=./libs/stb
 
 # curl all headers
-curl $STB_IMAGE > $OUTPUT/stb_image.h
+curl $STB_IMAGE_H > $OUTPUT/stb_image.h
+curl $STB_LICENSE > $OUTPUT/LICENSE
