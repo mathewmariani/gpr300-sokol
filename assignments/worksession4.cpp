8a161991ee6b9a4901e410feecca52994db1c3ab
Mat Mariani <someone.mariani@gmail.com>
2024-08-31 15:49:57 -0400

    Updated worksession4

--- assignments/worksession4.cpp
+++ assignments/worksession4.cpp
@@ -8,11 +8,15 @@
 #include "batteries/assets.h"
 #include "batteries/camera.h"
 #include "batteries/model.h"
-#include "batteries/materials.h"
 #include "batteries/lights.h"
+
+#include "batteries/framebuffer.h"
+#include "batteries/gizmo.h"
 
 // shaders
 #include "batteries/shaders/toon_shadowing.h"
+
+static constexpr glm::vec4 light_orbit_radius = glm::vec4(2.0f, 0.0f, 2.0f, 1.0f);
 
 typedef struct
 {
@@ -28,58 +32,57 @@ typedef struct
 
 typedef struct
 {
-    batteries::ambient_t ambient;
+    batteries::light_t light;
     palette_t palette;
 } fs_display_params_t;
+
+static palette_t pal[]{
+    {.highlight = glm::vec3(1.00f, 1.00f, 1.00f), .shadow = glm::vec3(0.60f, 0.54f, 0.52f)},
+    {.highlight = glm::vec3(0.47f, 0.58f, 0.68f), .shadow = glm::vec3(0.32f, 0.39f, 0.57f)},
+    {.highlight = glm::vec3(0.62f, 0.69f, 0.67f), .shadow = glm::vec3(0.50f, 0.55f, 0.50f)},
+    {.highlight = glm::vec3(0.24f, 0.36f, 0.54f), .shadow = glm::vec3(0.25f, 0.31f, 0.31f)},
+};
 
 // application state
 static struct
 {
     uint8_t file_buffer[boilerplate::megabytes(5)];
 
-    palette_t pal[4];
+    batteries::framebuffer_t framebuffer;
+    batteries::gizmo_t gizmo;
+
+    batteries::camera_t camera;
+    batteries::camera_controller_t camera_controller;
+
+    float ry;
 
     struct
     {
-        sg_pass_action pass_action;
-        sg_pass pass;
+        sg_pass_action action;
         sg_pipeline pip;
         sg_bindings bind;
     } object_pass;
 
     struct
     {
-        float ry;
-        batteries::ambient_t ambient;
+        batteries::light_t light;
         palette_t palette;
-
         struct
         {
-            batteries::model_t model;
             sg_image albedo;
             sg_image zatoon;
+            batteries::model_t model;
         } skull;
     } scene;
-
-    batteries::camera_t camera;
-    batteries::camera_controller_t camera_controller;
 } state = {
-    .pal = {
-        {.highlight = glm::vec3(1.00f, 1.00f, 1.00f), .shadow = glm::vec3(0.60f, 0.54f, 0.52f)},
-        {.highlight = glm::vec3(0.47f, 0.58f, 0.68f), .shadow = glm::vec3(0.32f, 0.39f, 0.57f)},
-        {.highlight = glm::vec3(0.62f, 0.69f, 0.67f), .shadow = glm::vec3(0.50f, 0.55f, 0.50f)},
-        {.highlight = glm::vec3(0.24f, 0.36f, 0.54f), .shadow = glm::vec3(0.25f, 0.31f, 0.31f)},
-    },
+    .ry = 0.0f,
     .scene = {
-        .ry = 0.0f,
-        .ambient = {
-            .color = glm::vec3(0.25f, 0.45f, 0.65f),
-            .intensity = 1.0f,
+        .light = {
+            .brightness = 1.0f,
+            .color = {1.0f, 1.0f, 1.0f},
+            .position = {10.0f, 10.0f, 0.0f},
         },
-        .palette = {
-            .highlight = glm::vec3(1.00f, 1.00f, 1.00f),
-            .shadow = glm::vec3(0.60f, 0.54f, 0.52f),
-        },
+        .palette = pal[0],
     },
 };
 
@@ -126,9 +129,9 @@ void create_object_pass(void)
                 .layout = SG_UNIFORMLAYOUT_NATIVE,
                 .size = sizeof(fs_display_params_t),
                 .uniforms = {
-                    [0] = {.name = "ambient.intensity", .type = SG_UNIFORMTYPE_FLOAT},
-                    [1] = {.name = "ambient.direction", .type = SG_UNIFORMTYPE_FLOAT3},
-                    [2] = {.name = "ambient.color", .type = SG_UNIFORMTYPE_FLOAT3},
+                    [0] = {.name = "light.brightness", .type = SG_UNIFORMTYPE_FLOAT},
+                    [1] = {.name = "light.color", .type = SG_UNIFORMTYPE_FLOAT3},
+                    [2] = {.name = "light.position", .type = SG_UNIFORMTYPE_FLOAT3},
                     [3] = {.name = "palette.highlight", .type = SG_UNIFORMTYPE_FLOAT3},
                     [4] = {.name = "palette.shadow", .type = SG_UNIFORMTYPE_FLOAT3},
                 },
@@ -157,9 +160,9 @@ void create_object_pass(void)
         },
     };
 
-    state.object_pass.pass_action = (sg_pass_action){
+    state.object_pass.action = (sg_pass_action){
         .colors[0] = {
-            .clear_value = {state.scene.ambient.color.r, state.scene.ambient.color.g, state.scene.ambient.color.b, 1.0f},
+            .clear_value = {0.0f, 0.0f, 0.0f, 1.0f},
             .load_action = SG_LOADACTION_CLEAR,
         },
     };
@@ -176,21 +179,31 @@ void create_object_pass(void)
         .index_type = SG_INDEXTYPE_NONE,
         .face_winding = SG_FACEWINDING_CCW,
         .cull_mode = SG_CULLMODE_BACK,
+        .colors = {
+            [0].pixel_format = SG_PIXELFORMAT_RGBA8,
+        },
         .depth = {
+            .pixel_format = SG_PIXELFORMAT_DEPTH,
             .compare = SG_COMPAREFUNC_LESS_EQUAL,
             .write_enabled = true,
         },
         .label = "object-pipeline",
     });
+}
 
-    auto smp = sg_make_sampler({
-        .min_filter = SG_FILTER_LINEAR,
-        .mag_filter = SG_FILTER_LINEAR,
-        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
-        .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-        .label = "object-sampler",
-    });
+void init(void)
+{
+    const auto width = sapp_width();
+    const auto height = sapp_height();
 
+    boilerplate::setup();
+    batteries::create_framebuffer(&state.framebuffer, width, height);
+    batteries::create_gizmo_pass(&state.gizmo);
+
+    load_skull();
+    create_object_pass();
+
+    // create bindings
     state.object_pass.bind = (sg_bindings){
         .vertex_buffers[0] = state.scene.skull.model.mesh.vbuf,
         .fs = {
@@ -198,17 +211,15 @@ void create_object_pass(void)
                 [0] = state.scene.skull.albedo,
                 [1] = state.scene.skull.zatoon,
             },
-            .samplers[0] = smp,
+            .samplers[0] = sg_make_sampler({
+                .min_filter = SG_FILTER_LINEAR,
+                .mag_filter = SG_FILTER_LINEAR,
+                .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
+                .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
+                .label = "object-sampler",
+            }),
         },
     };
-}
-
-void init(void)
-{
-    boilerplate::setup();
-
-    load_skull();
-    create_object_pass();
 }
 
 void draw_ui(void)
@@ -219,19 +230,19 @@ void draw_ui(void)
     ImGui::Text("Presets");
     if (ImGui::Button("Sunny Day"))
     {
-        state.scene.palette = state.pal[0];
+        state.scene.palette = pal[0];
     }
     if (ImGui::Button("Bright Night"))
     {
-        state.scene.palette = state.pal[1];
+        state.scene.palette = pal[1];
     }
     if (ImGui::Button("Rainy Day"))
     {
-        state.scene.palette = state.pal[2];
+        state.scene.palette = pal[2];
     }
     if (ImGui::Button("Rainy Night"))
     {
-        state.scene.palette = state.pal[3];
+        state.scene.palette = pal[3];
     }
 
     ImGui::Text("Palette");
@@ -248,15 +259,18 @@ void frame(void)
 
     const auto t = (float)sapp_frame_duration();
     state.camera_controller.update(&state.camera, t);
+    state.ry += 0.2f * t;
 
     // math required by the scene
     const auto camera_view_proj = state.camera.projection() * state.camera.view();
-    const auto light_pos = glm::vec4(10.0f, 10.0f, 0.0f, 0.0f);
-    state.scene.ambient.direction = glm::normalize(light_pos);
 
     // sugar: rotate and scale
-    state.scene.skull.model.transform.rotation = glm::rotate(state.scene.skull.model.transform.rotation, t, glm::vec3(0.0, 1.0, 0.0));
+    // state.scene.skull.model.transform.rotation = glm::rotate(state.scene.skull.model.transform.rotation, t, glm::vec3(0.0, 1.0, 0.0));
     state.scene.skull.model.transform.scale = glm::vec3(0.05f);
+
+    // sugar: rotate light
+    const auto rym = glm::rotate(state.ry, glm::vec3(0.0f, 1.0f, 0.0f));
+    state.scene.light.position = rym * light_orbit_radius;
 
     // initialize uniform data
     const vs_display_params_t vs_params = {
@@ -264,17 +278,40 @@ void frame(void)
         .model = state.scene.skull.model.transform.matrix(),
     };
     const fs_display_params_t fs_params = {
-        .ambient = state.scene.ambient,
+        .light = state.scene.light,
         .palette = state.scene.palette,
+    };
+    const batteries::vs_gizmo_params_t vs_gizmo_params = {
+        .view_proj = camera_view_proj,
+        .model = glm::translate(glm::mat4(1.0f), state.scene.light.position),
+    };
+    const batteries::fs_gizmo_light_params_t fs_gizmo_light_params = {
+        .light_color = state.scene.palette.highlight,
     };
 
     // graphics pass
-    sg_begin_pass({.action = state.object_pass.pass_action, .swapchain = sglue_swapchain()});
+    sg_begin_pass({.action = state.object_pass.action, .attachments = state.framebuffer.attachments});
     sg_apply_pipeline(state.object_pass.pip);
     sg_apply_bindings(&state.object_pass.bind);
     sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, SG_RANGE(vs_params));
     sg_apply_uniforms(SG_SHADERSTAGE_FS, 0, SG_RANGE(fs_params));
     sg_draw(0, state.scene.skull.model.mesh.num_faces * 3, 1);
+    sg_end_pass();
+
+    // render light sources
+    sg_begin_pass({.action = state.gizmo.action, .attachments = state.framebuffer.attachments});
+    sg_apply_pipeline(state.gizmo.pip);
+    sg_apply_bindings(&state.gizmo.bind);
+    sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, SG_RANGE(vs_gizmo_params));
+    sg_apply_uniforms(SG_SHADERSTAGE_FS, 0, SG_RANGE(fs_gizmo_light_params));
+    sg_draw(state.gizmo.sphere.base_element, state.gizmo.sphere.num_elements, 1);
+    sg_end_pass();
+
+    // display pass
+    sg_begin_pass({.action = state.framebuffer.action, .swapchain = sglue_swapchain()});
+    sg_apply_pipeline(state.framebuffer.pip);
+    sg_apply_bindings(&state.framebuffer.bind);
+    sg_draw(0, 6, 1);
 
     // draw ui
     simgui_render();
