0594b978e22b07ff9511945fe65789b06a691a23
Mat Mariani <someone.mariani@gmail.com>
2025-01-24 21:24:50 -0500

    Updated vscode settings

--- extra/cmake/macros.cmake
+++ extra/cmake/macros.cmake
@@ -1,13 +1,20 @@
 macro(emscripten target)
   if (CMAKE_SYSTEM_NAME STREQUAL Emscripten)
+    # if (CMAKE_BUILD_TYPE STREQUAL "Release")
+    #   set(CMAKE_EXECUTABLE_SUFFIX ".js")
+    # else()
+    #   set(CMAKE_EXECUTABLE_SUFFIX ".html")
+    # endif()
+
     set(CMAKE_EXECUTABLE_SUFFIX ".html")
     target_link_options(${target} PRIVATE
+      --shell-file ../extra/shell.html
       -sINITIAL_MEMORY=50MB
       -sMAXIMUM_MEMORY=200MB
       -sALLOW_MEMORY_GROWTH=1
       -sUSE_WEBGL2=1
       $<$<CONFIG:Debug>:-g>
-      $<$<CONFIG:Debug>:--shell-file ../extra/shell.html>)
+      $<$<CONFIG:Debug>:-sSINGLE_FILE=1>)
   endif()
 endmacro()
 
