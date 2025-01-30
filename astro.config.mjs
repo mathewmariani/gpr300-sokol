bb6682b4f6da3c35a790711f30af8323c7072fcf
Mat Mariani <someone.mariani@gmail.com>
2025-01-30 13:27:46 -0500

    Added astro integration for symlinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { symlinkIntegration } from './website/src/integrations/symlink';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [symlinkIntegration()],
     site: 'https://www.mathewmariani.com',
     base: '/gpr300-sokol/',
     srcDir: './website/src',
---
+++ website/src/integrations/symlink.ts
@@ -0,0 +1,36 @@
+import type { AstroIntegration } from "astro";
+import fs from 'fs';
+import path from 'path';
+
+export const symlinkIntegration = () => {
+  const symlinks = [
+    { source: 'build/demos/Release', target: 'website/public/demos' },
+    { source: 'assets', target: 'website/public/assets' },
+  ] as const;
+
+  let integration: AstroIntegration = {
+    name: "astro-lifecycle-logs",
+    hooks: {
+      'astro:build:done': async () => {
+        try {
+          for (const symlink of symlinks) {
+            const sourcePath = path.resolve(process.cwd(), symlink.source);
+            const targetPath = path.resolve(process.cwd(), symlink.target);
+            if (!fs.existsSync(targetPath)) {
+              fs.symlinkSync(sourcePath, targetPath, 'dir');
+              console.log(`Symlink created: ${sourcePath} -> ${targetPath}`);
+            } else {
+              console.log(`Symlink already exists: ${targetPath}`);
+            }
+          }
+        } catch (error: any) {
+          console.log(`Failed to create symlink: ${error.message}`);
+        }
+      },
+    },
+  };
+
+  return integration;
+};
+
+export default symlinkIntegration;
