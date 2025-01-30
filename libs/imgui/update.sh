a9638a53bfa33e653ac615fc874e78f210fce8d7
Mat Mariani <someone.mariani@gmail.com>
2025-01-30 13:29:01 -0500

    Removed uneeded imgui_demo.cpp

--- libs/imgui/update.sh
+++ libs/imgui/update.sh
@@ -6,7 +6,6 @@ IMGUI_PREFIX="https://raw.githubusercontent.com/ocornut/imgui/master"
 
 # headers
 IMGUI_CONFIG_H="$IMGUI_PREFIX/imconfig.h"
-IMGUI_DEMO_CPP="$IMGUI_PREFIX/imgui_demo.cpp"
 IMGUI_DRAW_CPP="$IMGUI_PREFIX/imgui_draw.cpp"
 IMGUI_INTERNAL_H="$IMGUI_PREFIX/imgui_internal.h"
 IMGUI_TABLES_CPP="$IMGUI_PREFIX/imgui_tables.cpp"
@@ -25,7 +24,6 @@ OUTPUT=./libs/imgui
 
 # curl all files
 curl $IMGUI_CONFIG_H > $OUTPUT/imconfig.h
-curl $IMGUI_DEMO_CPP > $OUTPUT/imgui_demo.cpp
 curl $IMGUI_DRAW_CPP > $OUTPUT/imgui_draw.cpp
 curl $IMGUI_INTERNAL_H > $OUTPUT/imgui_internal.h
 curl $IMGUI_TABLES_CPP > $OUTPUT/imgui_tables.cpp
