4bf8da122da9e022ce936e785733a2fa577a1bc5
mathewmariani <someone.mariani@gmail.com>
2025-02-06 15:44:10 -0500

    Added assignment1

--- assets/shaders/postprocess/grayscale.fs
+++ assets/shaders/postprocess/grayscale.fs
@@ -4,13 +4,13 @@ precision mediump float;
 
 out vec4 FragColor;
 
-in vec2 TexCoords;
+in vec2 vs_texcoord;
 
 uniform sampler2D screen;
 
 void main()
 {
-  vec3 color = texture(screen, TexCoords).rgb;
+  vec3 color = texture(screen, vs_texcoord).rgb;
   float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
   FragColor = vec4(average, average, average, 1.0);
 }
