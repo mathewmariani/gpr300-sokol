20f75fdaee5f60cce8df82755e65c4c21810793d
mathewmariani <someone.mariani@gmail.com>
2025-02-07 11:20:32 -0500

    Added worksession5

--- assets/shaders/transition.fs
+++ assets/shaders/transition.fs
@@ -4,20 +4,24 @@ precision mediump float;
 
 struct Transition
 {
-  sampler2D gradient;
   float cutoff;
   vec3 color;
 };
 
 out vec4 FragColor;
 
-in vec2 texcoords;
+in vec2 vs_texcoords;
 
+uniform sampler2D gradient;
 uniform Transition transition;
 
 void main()
 {
-  float transit = texture(transition.gradient, texcoords).r;
-  if (transit >= transition.cutoff) { discard; }
+  float transit = texture(gradient, vs_texcoords).r;
+  if (transit >= transition.cutoff)
+  {
+    discard;
+  }
+  
   FragColor = vec4(transition.color, 1.0);
 }
