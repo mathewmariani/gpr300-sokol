777aa14d5d6b05f0310714a14f7087daf767bacb
mathewmariani <someone.mariani@gmail.com>
2025-02-07 14:43:48 -0500

    Updated blinnphong shader

--- assets/shaders/blinnphong.fs
+++ assets/shaders/blinnphong.fs
@@ -11,10 +11,8 @@ struct Material {
 struct Ambient {
   float intensity;
   vec3 color;
-  vec3 direction;
 };
 struct Light {
-  float brightness;
   vec3 color;
   vec3 position;
 };
@@ -37,28 +35,26 @@ vec3 blinnPhong(vec3 normal, vec3 frag_pos, vec3 light_pos, vec3 light_color) {
   // normalize inputs
   vec3 view_dir = normalize(camera_position - frag_pos);
   vec3 light_dir = normalize(light_pos - frag_pos);
-  vec3 halfway_dir = normalize(light_dir + view_dir);  
+  vec3 halfway_dir = normalize(light_dir + view_dir);
 
-  // diffuse component
+  // dot products
   float ndotl = max(dot(normal, light_dir), 0.0);
-  vec3 diffuse = ndotl * light_color * material.diffuse;
+  float ndoth = max(dot(normal, halfway_dir), 0.0);
 
-  // specular component
-  float spec = 0.0;
-  if (ndotl > 0.0) {
-    spec = pow(max(dot(normal, halfway_dir), 0.0), material.shininess);
-  }
-  vec3 specular = spec * light_color * material.specular;
+  // components
+  vec3 diffuse = ndotl * material.diffuse;
+  vec3 specular = pow(ndoth, material.shininess * 128.0) * material.specular;
 
-  return diffuse + specular;
+  return (diffuse + specular) * light_color;
 }
 
 void main()
 {
   vec3 normal = normalize(vs_normal);
-  vec3 object_color = texture(texture0, vs_texcoord).rgb;
+  // vec3 object_color = texture(texture0, vs_texcoord).rgb;
+  vec3 object_color = (normal * 0.5 + 0.5);
   vec3 light_color = blinnPhong(normal, vs_position, light.position, light.color);
-  light_color += ambient.intensity * ambient.color * material.ambient;
+  light_color += ambient.color * material.ambient;
 
   FragColor = vec4(object_color * light_color, 1.0);
 }
