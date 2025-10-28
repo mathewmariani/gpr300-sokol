f9d69b2fadf80e90df21d3c8ef26295052352f40
Mathew Mariani <mat.mariani@hotmail.com>
2025-10-28 11:33:15 -0400

    Removed emscripten shell

--- extra/cmake/macros.cmake
+++ extra/cmake/macros.cmake
@@ -2,7 +2,6 @@ macro(emscripten target)
   if (CMAKE_SYSTEM_NAME STREQUAL Emscripten)
     set(CMAKE_EXECUTABLE_SUFFIX ".js")
     target_link_options(${target} PRIVATE
-      --shell-file ../extra/shell.html
       -sINITIAL_MEMORY=50MB
       -sMAXIMUM_MEMORY=200MB
       -sALLOW_MEMORY_GROWTH=1
