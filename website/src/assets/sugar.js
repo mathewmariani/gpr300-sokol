06f2359e46f2e1bdebff20d258abb898c1337d81
mathewmariani <someone.mariani@gmail.com>
2026-03-26 11:07:13 -0400

    Updated website

--- website/src/assets/sugar.js
+++
@@ -1,8 +0,0 @@
-// https://flatuicolors.com/palette/ca
-const palette = ["#ff9ff3", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1"];
-window.addEventListener("mousedown", (e) => {
-  const color = palette.shift();
-  //document.documentElement.style.setProperty("--highlight-color", color);
-  document.documentElement.style.setProperty("--highlight-background", color);
-  palette.push(color);
-});
--- website/src/consts.ts
+++ website/src/consts.ts
@@ -1,6 +1,11 @@
 export const SITE_TITLE = 'GPR-300';
 export const SITE_DESCRIPTION = '';
 
+export const HomeLink = (): string => '/';
+export const ResumeLink = (): string => '/resume';
+export const ProjectsLink = (): string => '/projects';
+export const CoursesLink = (): string => '/courses';
+
 export const SYMLINKS = [
   { source: `build/web/demos`, target: `website/public/demos` },
 ] as const;
